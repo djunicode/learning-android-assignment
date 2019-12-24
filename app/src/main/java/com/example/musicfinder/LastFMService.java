086f42b49afe0d29cd6758f49a66f18f99034d82
MihirS57 <shahmihir507@gmail.com>
2019-12-24 14:32:50 +0530

    Track Search Implemented

--- app/src/main/java/com/example/musicfinder/LastFMService.java
+++
@@ -1,5 +0,0 @@
-package com.example.musicfinder;
-
-public interface LastFMService {
-
-}
--- app/src/main/java/com/example/musicfinder/MainActivity.java
+++ app/src/main/java/com/example/musicfinder/MainActivity.java
@@ -1,15 +1,51 @@
 package com.example.musicfinder;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navlistener);
+
+        loadFragment(new TrackSearch_Fragment());
 
     }
+    BottomNavigationView.OnNavigationItemSelectedListener navlistener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            Fragment fragment = null;
+
+            switch (menuItem.getItemId())
+            {
+                case R.id.Track_search:
+                    fragment = new TrackSearch_Fragment();
+                    break;
+            }
+                loadFragment(fragment);
+            return false;
+        }
+    };
+
+    public void loadFragment(Fragment frag)
+    {
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container,frag,null);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
 }
